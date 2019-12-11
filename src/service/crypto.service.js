ace5d9b376568ac060283c6ec0cfd33ec80f51b7
Jackson Bi <lw81240@linecorp.com>
2019-12-11 18:37:15 +0700

    - graphql features

--- src/service/crypto.service.js
+++ src/service/crypto.service.js
@@ -2,11 +2,11 @@ import cryptoJS from 'crypto-js';
 
 const secretKey = 'vn425282';
 class CryptoService {
-    encrypt (token) {
+    encrypt(token) {
         return cryptoJS.AES.encrypt(token, secretKey).toString();
     }
 
-    decrypt (hash) {
+    decrypt(hash) {
         const bytes  = cryptoJS.AES.decrypt(hash, secretKey);
         return bytes.toString(cryptoJS.enc.Utf8);
     }
--- src/service/github.service.js
+++ src/service/github.service.js
@@ -1,9 +1,12 @@
 import baseService from './base.service';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
 
 const configDefault = {
     clientID: '8a91cd834c4ea7e246cd',
     redirectURL: 'http://localhost:3000/',
-    herokuappAuthURL: 'https://react-github-finduser.herokuapp.com/authenticate'
+    herokuappAuthURL: 'https://react-github-finduser.herokuapp.com/authenticate',
+    gitGraphqlURL: 'https://api.github.com/graphql'
 };
 
 class GitHubService {
@@ -17,6 +20,29 @@ class GitHubService {
 
     getRedirectURL(){
         return configDefault.redirectURL;
+    }
+
+    getClient(token){
+        const httpLink = new HttpLink({
+            uri: configDefault.gitGraphqlURL,
+            headers: {
+              Authorization: `bearer ${token}`,
+            },
+          });
+
+        const errorLink = onError(({ graphQLErrors, networkError }) => {
+            if (graphQLErrors) {
+              graphQLErrors.map(({ message, locations, path }) =>
+                console.log(
+                  `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+                ),
+              );
+            }
+
+            if (networkError) {
+              console.log(`[Network error]: ${networkError}`);
+            }
+        });
     }
 }
 
